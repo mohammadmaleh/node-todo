2268e75dcaf4ec2ff593e7803f0a91dc42950375
mohammad maleh <mohammadmaleh@mohammad-MacBook-Pro.local>
2017-06-11 04:18:44 +0300

    adding post user rout and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,53 @@
-var mongoose = require('mongoose')
+const mongoose = require('mongoose')
+const validator = require('validator')
+const jwt = require('jsonwebtoken')
+const _ = require('lodash')
+// to add methods to our model we can do directly so we use schema then add the method then add the whole thing to the model
+let UserSchema =  new mongoose.Schema({
+        email:{
+            type:String,
+            unique:true,
+            trim:true,
+            required:true,
+            minlength:1,
+            validate:{ // custom validation
+                validator:(value)=>{ // return true if valid return false if not
+                    return validator.isEmail(value)
+                },
+                message:'{VALUE} is not a valid email'
 
-var User = mongoose.model('User', {
-    email:{
-        type:String,
-        trim:true,
-        required:true,
-        minlength:1
-    }
-})
+
+            }
+        },
+        password:{
+            type: String,
+            required:true,
+            minlength:6
+        },
+        tokens:[{
+            access:{
+                type:String,
+                required:true
+            },
+            token:{
+                type:String,
+                required:true
+            }
+        }]
+    })
+UserSchema.methods.toJSON = function(){ // overwriting an existing function to downsize what are we returning
+ let user = this;
+ let userObject = this.toObject()
+    return _.pick(userObject,['_id','email'])
+}
+UserSchema.methods.generateAuthToken =function () { // we used old function because arrow function doesn't bind this
+    let user = this ;
+    let access = 'auth';
+    let token = jwt.sign({_id : user._id.toHexString(),access:access},'kappa123').toString();
+    user.tokens.push({access:access,token:token})
+    return user.save().then(()=>{
+        return token
+    })
+}
+var User = mongoose.model('User', UserSchema )
 module.exports = {User}
